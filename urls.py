73404c4c816f3ef0715c41eb4a1dc24c866a6deb
Jason Frerich <jasonfrerich@yahoo.com>
2019-01-17 18:32:53 -0600

    inital add of realty html files

--- urls.py
+++ urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-# from . import views
+from . import views
 from .views import TestModelList, TestModelListJson
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('viewDb/', views.viewDb, name='viewDb'),
+    path('overview/', views.overview, name='overview'),
     path('testmodel/', TestModelList.as_view(), name='testmodel'),
     path('testmodel_data/', TestModelListJson.as_view(),
          name="testmodel_list_json"),
--- views.py
+++ views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from django.views.generic import TemplateView
 from django_datatables_view.base_datatable_view import BaseDatatableView
 
 from realty.models import TestModel
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'realty/index.html')
+    # template_name = 'realty/viewDb.html'
+
+
+def viewDb(request):
+    print("In Here!")
+    return render(request, 'viewDb.html')
+    # template_name = 'realty/viewDb.html'
+
+
+def overview(request):
+    return render(request, 'realty/overview.html')
+
 
 class TestModelList(TemplateView):
     template_name = 'realty/testmodel_list.html'
     # template_name = 'realty/viewDb.html'
 
+
 class TestModelListJson(BaseDatatableView):
 
     model = TestModel
-    # columns and order columns are provided by datatables in the request using "name" in columns definition
-
+    # columns and order columns are provided by datatables in the request using
+    # "name" in columns definition
