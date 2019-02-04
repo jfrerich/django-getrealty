7ace0acd9a5679d4c46b893a235ed97beb6f4197
Jason Frerich <jasonfrerich@yahoo.com>
2019-02-04 08:56:16 -0600

    Remove unused urls and comments

--- realty/urls.py
+++ realty/urls.py
@@ -2,28 +2,22 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import TemplateView
 
 from . import views
-# from .views import ViewDbList, ViewDbListJson, ZeroConfigurationDatatableView
-from .views import ViewDbListJson, ZeroConfigurationDatatableView, detailview
+from .views import ZeroConfigurationDatatableView, detailview
 
 urlpatterns = [
     path('', views.home, name='realty'),
     path('viewDb/', views.viewDb, name='viewDb'),
-    path('viewDb_data/', ViewDbListJson.as_view(), name="viewdb_list_json"),
-
-    # path('viewDb/', views.modelDb, name='modelDb'),
-    # path('javascript-initialization/', views.JavascriptInitializationView.as_view(), name="js-init"),
-
-    # path('viewModelDb/', views.viewModelDb, name="viewModelDb"),
-    path('viewModelDb_data/', csrf_exempt(ZeroConfigurationDatatableView.as_view()), name="viewModelDb_data"),
-    # path('viewModelDb_data/', views.ZeroConfigurationDatatableView, name="viewModelDb_list_json"),
-
+    path('viewModelDb_data/',
+         csrf_exempt(ZeroConfigurationDatatableView.as_view()),
+         name="viewModelDb_data"),
     path('overview/', views.overview, name='overview'),
     path('query/', views.query, name='query'),
     # path('detail/', views.detail, name='detail'),
-    # path('detail/', detailview.as_view(template_name='realty/propertydetail.html'), name='detail'),
+    # path('detail/',
+    #      detailview.as_view(template_name='realty/propertydetail.html'),
+    #      name='detail'),
     path('detail/', detailview.as_view(), name='detail'),
     path('testpage/', views.testpage, name='testpage'),
     path('testPage2/', views.testPage2, name='testPage2'),
